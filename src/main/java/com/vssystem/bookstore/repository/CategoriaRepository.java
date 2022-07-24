6e8a46c67007eabf4f827a2adff95a56f1050766
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2022-07-24 13:53:39 -0300

    Criando as Classes de Repository

---
+++ src/main/java/com/vssystem/bookstore/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.vssystem.bookstore.repository;
+
+import com.vssystem.bookstore.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
